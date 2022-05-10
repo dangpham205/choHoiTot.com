100b214efc5c304912acd1be3812f2d789614ccb
Dawnny <haidawng205@gmail.com>
2022-05-10 13:56:23 +0700

    edit profile

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError, Regexp
+
+from ..models import User
+
 
 class ChangePassForm(FlaskForm):
     old_password = PasswordField('Old password', validators=[DataRequired()])
@@ -8,3 +12,17 @@ class ChangePassForm(FlaskForm):
         DataRequired(), EqualTo('new_password2', message='Passwords must match.')])
     new_password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Change Password')
+
+class EditProfileForm(FlaskForm):
+    def validate_username(self,username_to_check):
+        user = User.query.filter_by(user_name=username_to_check.data).first()       #phải có .data
+        if user:
+            raise ValidationError('Username already exists')
+
+    username = StringField('Username:', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+               'Usernames must have only letters, numbers, dots or '
+               'underscores')])
+    fullname = StringField(label='Full Name:', validators=[Length(min=2, max=40), DataRequired()])
+    phone = StringField(label='Phone Number:', validators=[Length(min=8, max=12), DataRequired()])
+    bio = StringField(label='About Me:')
+    submit = SubmitField(label='Update')
--- app/profile/views.py
+++ app/profile/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import render_template, redirect, url_for, flash, request, session
 
-from app.profile.forms import ChangePassForm
+from app.profile.forms import ChangePassForm, EditProfileForm
 from ..models import Student, User
 from .. import db
 from hashlib import md5
@@ -9,15 +9,33 @@ from flask_login import login_required, current_user
 from . import profile
 from ..email import send_email, send_congrat_email
 
-@profile.route('/profile/<username>')
+@profile.route('/<username>')
 def profile_page(username):
     if username is not None:        
         user = User.query.filter_by(user_name = username).first_or_404()
         email= user.user_email
         email_hash = md5(email.encode("utf-8")).hexdigest()
-        user_avatar = f"https://www.gravatar.com/avatar/{email_hash}?s=200"
+        user_avatar = f"https://www.gravatar.com/avatar/{email_hash}?s=100&d=https://lolslaves.com/wp-content/uploads/2020/01/3.png"
         # role_id = str(current_user.role)
         return render_template('profile/profile.html', user=user, user_avatar=user_avatar)
+
+@profile.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    user = User.query.filter_by(user_name = current_user.user_name).first()
+    if form.validate_on_submit():
+        user.user_name = form.username.data
+        user.user_fullname = form.fullname.data
+        user.user_phone = form.phone.data
+        user.bio = form.bio.data
+        db.session.commit()
+        flash('Update profile succeed !', category='success')
+        return redirect(url_for('profile.profile_page', username=current_user.user_name))
+    if form.errors != {}:       #if there are errors
+        for error in form.errors.values():
+            flash(f'{error}', category='danger')
+    return render_template('profile/edit_profile.html', user=user, form=form)
 
 
 @profile.route('/change_password', methods=['GET', 'POST'])
