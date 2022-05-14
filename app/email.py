a37b29835d5e63f119d51f5c588906778e0f16df
Dawnny <haidawng205@gmail.com>
2022-05-14 10:52:44 +0700

    email

--- app/email.py
+++ app/email.py
@@ -11,8 +11,8 @@ def send_async_email(app, msg):
 
 def send_email(to, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message("TDTU iBanking",
-                  sender='Flasky Admin <flasky@example.com>', recipients=[to])
+    msg = Message("choHoiTot.com",
+                  sender='Team8 Admin <team8@tdtu.com>', recipients=[to])
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
