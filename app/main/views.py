22e0dc3f7f2ea7f7a89c11f91ef0ee353c2a5ba5
Dawnny <haidawng205@gmail.com>
2022-05-16 20:36:41 +0700

    load image

--- app/main/views.py
+++ app/main/views.py
@@ -64,7 +64,7 @@ def detail_page(product_id):
         product = Product.query.filter_by(id=product_id).first()
         owner = User.query.filter_by(id=product.owner_id).first()
         others = Product.query.filter(Product.category == product.category,
-                                    Product.owner_id != current_user.id,
+                                    # Product.owner_id != current_user.id,
                                     Product.id != product.id).limit(7).all()
     return render_template('market/product_detail.html', 
                             product = product , 
